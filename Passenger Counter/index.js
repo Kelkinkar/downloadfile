22f6bb2fc49a33d4cf2fc69fb31303de25855e85
Kelkinkar <kelkinkar@gmail.com>
2022-05-24 22:48:30 +0300

    my files

---
+++ Passenger Counter/index.js
@@ -0,0 +1,18 @@
+// document.getElementById("count").innertext = 5
+let count = 0
+console.log(count)
+
+//  Calculating dog age
+ let myAge = 23
+ let humanDogRatio =  7 
+ let myDogAge = myAge * humanDogRatio
+ console.log(myDogAge)
+
+ let bonuspoints = 50
+ bonuspoints += 50
+ console.log(bonuspoints)
+ bonuspoints -= 75
+ console.log(bonuspoints)
+ bonuspoints += 45
+ console.log(bonuspoints )
+
