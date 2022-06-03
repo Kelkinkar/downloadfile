6fab62a0f0d1eee0554ebfaa81028b17bca69f21
Kelkinkar <kelkinkar@gmail.com>
2022-06-03 14:37:47 +0300

    new files

--- Passenger Counter/index.js
+++ Passenger Counter/index.js
@@ -1,18 +1,38 @@
-// document.getElementById("count").innertext = 5
-let count = 0
-console.log(count)
+
 
 //  Calculating dog age
- let myAge = 23
- let humanDogRatio =  7 
- let myDogAge = myAge * humanDogRatio
- console.log(myDogAge)
+//  let myAge = 23
+//  let humanDogRatio =  7 
+//  let myDogAge = myAge * humanDogRatio
+//  console.log(myDogAge)
 
- let bonuspoints = 50
- bonuspoints += 50
- console.log(bonuspoints)
- bonuspoints -= 75
- console.log(bonuspoints)
- bonuspoints += 45
- console.log(bonuspoints )
+//  let bonuspoints = 50
+//  bonuspoints += 50
+//  console.log(bonuspoints) 
+//  bonuspoints -= 75
+//  console.log(bonuspoints)
+//  bonuspoints += 45 
+//  console.log(bonuspoints )
+let welcomeEl = document.getElementById("welcome-el")
+let name = "kelvin"
+let greeting = " Welcome Back "
+
+welcomeEl.innerText = greeting + name
+welcomeEl.innerText += " 🤑"
+
+let countEl = document.getElementById("count-el")
+let buttonClick = 0
+
+function increment(){
+    buttonClick += 1
+    countEl.innerText = buttonClick
+}
+
+increment() 
+
+function save(){
+    console.log(buttonClick) 
+    const data = 'Hello world'
+    writeFileSync('output.txt', data)
+}
 
