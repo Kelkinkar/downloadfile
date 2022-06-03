b0ed3b1dabeb9399593fbb7172e1879e30e4201c
Kelkinkar <kelkinkar@gmail.com>
2022-06-03 15:09:04 +0300

    new

--- Passenger Counter/index.js
+++ Passenger Counter/index.js
@@ -1,18 +1,41 @@
-// document.getElementById("count").innertext = 5
+  
+//  Calculating dog age
+
+//  let myAge = 23
+//  let humanDogRatio =  7 
+//  let myDogAge = myAge * humanDogRatio
+//  console.log(myDogAge)
+
+//  let bonuspoints = 50
+//  bonuspoints += 50
+//  console.log(bonuspoints)
+//  bonuspoints -= 75
+//  console.log(bonuspoints)
+//  bonuspoints += 45
+//  console.log(bonuspoints )
+
+let countEl = document.getElementById("count-el")
+
 let count = 0
+function increment(){
+    count  += 1
+    countEl.innerText = count     
+}
+
+increment()
+
+function save(){
 console.log(count)
 
-//  Calculating dog age
- let myAge = 23
- let humanDogRatio =  7 
- let myDogAge = myAge * humanDogRatio
- console.log(myDogAge)
+}
+save()
+ 
+let messageToUser = "You have three new notifications, per!"
 
- let bonuspoints = 50
- bonuspoints += 50
- console.log(bonuspoints)
- bonuspoints -= 75
- console.log(bonuspoints)
- bonuspoints += 45
- console.log(bonuspoints )
+
+let name  = "Kinuthia"
+let greeting = "Hi my name is Kelvin "
+
+let myGreeting = greeting + name 
+console.log(myGreeting)
 
